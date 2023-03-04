416dd672991fc877513531a5344fbc1a32f33ca5
Mitte <wmyameogo@songo-technologies.com>
2023-03-04 09:32:32 +0000

    Sat Mar  4 09:32:32 GMT 2023

--- docker-centos.sh
+++ docker-centos.sh
@@ -27,7 +27,7 @@ yum install -y ${_instance_core_paquets} ${_instance_other_paquets}
 ## CONFIGURATION DE DOCKER
 ####################################################################################################
 # Install Docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
